7d3786ca8a93bd37c6d7f4ffc682c6d04ba97ce1
Rune Anderberg <rune.anderberg@gmail.com>
2022-12-03 11:30:09 +0100

    Day3 execution time testing

--- Day3/src/Day3.java
+++ Day3/src/Day3.java
@@ -3,106 +3,56 @@ import java.util.Arrays;
 
 public class Day3 {
     public static void main(String[] args) throws IOException {
-        long startTime = System.nanoTime();
-        //calculatePartOne();
-        //calculatePartTwo();
-        calculate();
-        long elapsedTime = System.nanoTime() - startTime;
-        System.out.println("execution time (ms): " + elapsedTime/1000000);
+        //runParts();
+        runCombined();
+    }
+
+    private static void runParts() throws IOException {
+        long elapsedTimeSum = 0;
+        for (int i = 0; i < 1000; i++) {
+            long startTime = System.nanoTime();
+            calculatePartOne();
+            calculatePartTwo();
+            long elapsedTime = System.nanoTime() - startTime;
+            elapsedTimeSum += elapsedTime;
+        }
+        System.out.println("average execution time (ns): " + elapsedTimeSum/1000);
+    }
+
+    private static void runCombined() throws IOException {
+        long elapsedTimeSum = 0;
+        for (int i = 0; i < 1000; i++) {
+            long startTime = System.nanoTime();
+            calculate();
+            long elapsedTime = System.nanoTime() - startTime;
+            elapsedTimeSum += elapsedTime;
+        }
+        System.out.println("average execution time (ns): " + elapsedTimeSum/1000);
     }
 
     private static void calculate() throws IOException {
         var input = new File("inputs/day3.txt");
         var br = new BufferedReader(new FileReader(input));
 
-        boolean[] occurs = new boolean[52];
         boolean[] occursOne = new boolean[52];
         boolean[] occursTwo = new boolean[52];
         boolean[] occursThree = new boolean[52];
 
         String line;
-        String left;
-        String right;
         int sumP1 = 0;
         int sumP2 = 0;
 
         while ((line = br.readLine()) != null) {
             // Line n
-            left = line.substring(0, line.length() / 2);
-            right = line.substring(line.length() / 2);
-
-            for (var c : left.toCharArray()) {
-                int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
-                occursOne[index] = true;
-
-                occurs[index] = true;
-            }
-
-            for (var c : right.toCharArray()) {
-                int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
-                occursOne[index] = true;
-
-                if (occurs[index]) {
-                    sumP1 += index + 1;
-
-                    // To prevent duplicates
-                    occurs[index] = false;
-                }
-            }
-
-            Arrays.fill(occurs, false);
+            sumP1 += calculateLine(line, occursOne);
 
             // Line n+1
             line = br.readLine();
-            left = line.substring(0, line.length() / 2);
-            right = line.substring(line.length() / 2);
-
-            for (var c : left.toCharArray()) {
-                int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
-                occursTwo[index] = true;
-
-                occurs[index] = true;
-            }
-
-            for (var c : right.toCharArray()) {
-                int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
-                occursTwo[index] = true;
-
-                if (occurs[index]) {
-                    sumP1 += index + 1;
-
-                    // To prevent duplicates
-                    occurs[index] = false;
-                }
-            }
-
-            Arrays.fill(occurs, false);
+            sumP1 += calculateLine(line, occursTwo);
 
             // Line n+2
             line = br.readLine();
-            left = line.substring(0, line.length() / 2);
-            right = line.substring(line.length() / 2);
-
-            for (var c : left.toCharArray()) {
-                int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
-                occursThree[index] = true;
-
-                occurs[index] = true;
-            }
-
-            for (var c : right.toCharArray()) {
-                int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
-                occursThree[index] = true;
-
-                if (occurs[index]) {
-                    sumP1 += index + 1;
-
-                    // To prevent duplicates
-                    occurs[index] = false;
-                }
-            }
-
-            Arrays.fill(occurs, false);
+            sumP1 += calculateLine(line, occursThree);
 
             // Sum for part two
             for (int i = 0; i < 52; i++) {
@@ -119,6 +69,34 @@ public class Day3 {
 
         System.out.println(sumP1);
         System.out.println(sumP2);
+    }
+
+    private static int calculateLine(String line, boolean[] occursPart) {
+        String left = line.substring(0, line.length() / 2);
+        String right = line.substring(line.length() / 2);
+        boolean[] occurs = new boolean[52];
+        int sum = 0;
+
+        for (var c : left.toCharArray()) {
+            int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
+            occursPart[index] = true;
+
+            occurs[index] = true;
+        }
+
+        for (var c : right.toCharArray()) {
+            int index = c >= 'a' ? c - 'a' : c - 'A' + 26;
+            occursPart[index] = true;
+
+            if (occurs[index]) {
+                sum += index + 1;
+
+                // To prevent duplicates
+                occurs[index] = false;
+            }
+        }
+
+        return sum;
     }
 
     private static void calculatePartOne() throws IOException {
