c4dff2bfa7b69b46b70c4c7631289cc396aab9c0
Rune Anderberg <rune.anderberg@gmail.com>
2022-12-06 20:04:20 +0100

    Day6 but slightly faster

--- Day5/src/Day5Fast.java
+++ Day5/src/Day5Fast.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Stack;
-import java.util.regex.Pattern;
 
 public class Day5Fast {
     public static void main(String[] args) throws IOException {
