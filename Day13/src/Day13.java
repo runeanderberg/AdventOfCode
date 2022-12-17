abc36b54453e9f6d28ba4f74a45be28c0b253eb0
Rune Anderberg <rune.anderberg@gmail.com>
2022-12-17 12:04:27 +0100

    Day13 part one parse

--- Day13/src/Day13.java
+++ Day13/src/Day13.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Day13 {
@@ -10,7 +11,7 @@ public class Day13 {
         for (int i = 0; i < 1000; i++) {
             long startTime = System.nanoTime();
 
-            parseInput();
+            var pairs = parseInput();
 
             long elapsedTime = System.nanoTime() - startTime;
             elapsedTimeSum += elapsedTime;
@@ -19,29 +20,120 @@ public class Day13 {
     }
 
     private static List<Pair> parseInput() throws IOException {
-        var input = new java.io.File("inputs/examples/day13_ex.txt");
+        var input = new java.io.File("inputs/day13.txt");
         var br = new BufferedReader(new FileReader(input));
 
         String line;
         var pairs = new ArrayList<Pair>();
 
         while ((line = br.readLine()) != null) {
-            pairs.add(new Pair(line, br.readLine()));
+            var first = parseLine(line);
+            var second = parseLine(br.readLine());
+            pairs.add(new Pair(first, second));
             br.readLine();
         }
 
         return pairs;
     }
 
+    private static Array parseLine(String line) {
+        int value = 0;
+        boolean hasValue = false;
 
+        var array = new Array();
+        var current = array;
+        line = line.substring(1);
+
+        for (var c : line.toCharArray()) {
+            switch (c) {
+                case '[':
+                    var temp = new Array();
+                    temp.parent = current;
+                    current.value.add(temp);
+                    current = temp;
+                    value = 0;
+                    hasValue = false;
+                    break;
+                case ']':
+                    if (hasValue) {
+                        current.value.add(new Number(value));
+                        value = 0;
+                        hasValue = false;
+                    }
+                    current = current.parent;
+                    break;
+                case ',':
+                    if (hasValue) {
+                        current.value.add(new Number(value));
+                        value = 0;
+                        hasValue = false;
+                    }
+                    break;
+                default:
+                    hasValue = true;
+                    value *= 10;
+                    value += c - '0';
+                    break;
+            }
+        }
+
+        return array;
+    }
 }
 
 class Pair {
-    String first;
-    String second;
+    Array first;
+    Array second;
 
-    public Pair(String first, String second) {
+    public Pair(Array first, Array second) {
         this.first = first;
         this.second = second;
+    }
+}
+
+interface Item {
+    boolean isArray();
+    void print();
+}
+class Number implements Item {
+    public int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean isArray() {
+        return false;
+    }
+
+    @Override
+    public void print() {
+        System.out.print(value);
+    }
+}
+
+class Array implements Item {
+    public List<Item> value = new LinkedList<>();
+    public Array parent;
+
+    @Override
+    public boolean isArray() {
+        return true;
+    }
+
+    @Override
+    public void print() {
+        System.out.print('[');
+
+        if (!value.isEmpty()) {
+            value.get(0).print();
+            for (int i = 1; i < value.size(); i++) {
+                System.out.print(',');
+                value.get(i).print();
+            }
+        }
+
+        System.out.print(']');
     }
 }
