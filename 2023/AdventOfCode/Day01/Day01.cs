70d754aa2af0d6cc2a822258dbb4a704cae850e4
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-01 17:23:39 +0100

    2023 - Day 1 complete

---
+++ 2023/AdventOfCode/Day01/Day01.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+var lines = File.ReadLines("input.txt");
+
+var firstSum = lines
+    .Select(l => l.Where(c => c is >= '0' and <= '9').ToArray())
+    .Select(ca => int.Parse(ca[0].ToString() + ca[^1]))
+    .Sum();
+
+var replacementTableList = new Dictionary<string, string>
+{
+    { "one", "o1e" },
+    { "two", "t2o" },
+    { "three", "t3e" },
+    { "four", "f4r" },
+    { "five", "f5e" },
+    { "six", "s6x" },
+    { "seven", "s7n" },
+    { "eight", "e8t" },
+    { "nine", "n9e" }
+}.ToList();
+
+var secondSum = lines
+    .Select(l =>
+    {
+        var sb = new StringBuilder(l);
+        replacementTableList.ForEach(r => sb.Replace(r.Key, r.Value));
+        return sb.ToString();
+    })
+    .Select(l => l.Where(c => c is >= '0' and <= '9').ToArray())
+    .Select(ca => int.Parse(ca[0].ToString() + ca[^1]))
+    .Sum();
+
+Console.WriteLine($"First sum = {firstSum}, Second sum = {secondSum}");
