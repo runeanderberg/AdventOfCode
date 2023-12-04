1aead96d52b2970d146bef18c1e5856b5427b482
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-04 22:00:01 +0100

    Day04 array solution

--- 2023/AdventOfCode/Day04/Day04.cs
+++ 2023/AdventOfCode/Day04/Day04.cs
@@ -1,35 +1,32 @@
 ﻿var lines = File.ReadLines("input.txt").ToArray();
 
-var cards = new Dictionary<int, int>();
-            
-foreach (var line in lines)
-{
-    var input = line.Split(':');
-                
-    var id = int.Parse(input[0].Split(' ')[^1]);
-    var numbers = input[1].Split('|');
+var cards = new int[lines.Length];
 
+for (var i = 0; i < lines.Length; i++)
+{
+    var numbers = lines[i].Split(':')[1].Split('|');
+    
     var winningNumbers = numbers[0].Split(' ').Where(s => s != "").Select(int.Parse);
     var otherNumbers = numbers[1].Split(' ').Where(s => s != "").Select(int.Parse);
 
     var numMatches = otherNumbers.Count(number => winningNumbers.Any(n => n == number));
 
-    cards.Add(id, numMatches);
+    cards[i] = numMatches;
 }
 
-var firstSum = cards.Where(card => card.Value > 0).Sum(card => (int) Math.Pow(2, card.Value - 1));
+var firstSum = cards.Where(card => card > 0).Sum(card => (int) Math.Pow(2, card - 1));
 
-var ownedCards = new Dictionary<int, int>();
-cards.ToList().ForEach(card => ownedCards.Add(card.Key, 1));
+var ownedCards = new int[lines.Length];
+Array.Fill(ownedCards, 1);
 
-for (var i = 1; i <= ownedCards.Count; i++)
+for (var i = 0; i < ownedCards.Length; i++)
 {
-    for (var j = 1; j <= cards[i] && i + j <= ownedCards.Count; j++)
+    for (var j = 1; j <= cards[i] && i + j < ownedCards.Length; j++)
     {
         ownedCards[i + j] += ownedCards[i];
     }
 }
 
-var secondSum = ownedCards.Sum(card => card.Value);
+var secondSum = ownedCards.Sum(card => card);
 
 Console.WriteLine($"First sum = {firstSum}, Second sum = {secondSum}");
