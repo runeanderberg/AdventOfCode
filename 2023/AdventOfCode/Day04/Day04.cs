b89f2c441e7ea8fd0585c73d754999852b161a9a
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-04 16:27:23 +0100

    Converted Day04 to top level code

--- 2023/AdventOfCode/Day04/Day04.cs
+++ 2023/AdventOfCode/Day04/Day04.cs
@@ -1,41 +1,35 @@
-﻿namespace Day04
+﻿var lines = File.ReadLines("input.txt").ToArray();
+
+var cards = new Dictionary<int, int>();
+            
+foreach (var line in lines)
 {
-    public class Day03
+    var input = line.Split(':');
+                
+    var id = int.Parse(input[0].Split(' ')[^1]);
+    var numbers = input[1].Split('|');
+
+    var winningNumbers = numbers[0].Split(' ').Where(s => s != "").Select(int.Parse);
+    var otherNumbers = numbers[1].Split(' ').Where(s => s != "").Select(int.Parse);
+
+    var numMatches = otherNumbers.Count(number => winningNumbers.Any(n => n == number));
+
+    cards.Add(id, numMatches);
+}
+
+var firstSum = cards.Where(card => card.Value > 0).Sum(card => (int) Math.Pow(2, card.Value - 1));
+
+var ownedCards = new Dictionary<int, int>();
+cards.ToList().ForEach(card => ownedCards.Add(card.Key, 1));
+
+for (var i = 1; i <= ownedCards.Count; i++)
+{
+    for (var j = 1; j <= cards[i] && i + j <= ownedCards.Count; j++)
     {
-        public static void Main(string[] args)
-        {
-            var lines = File.ReadLines("input.txt").ToArray();
-
-            var cards = new Dictionary<int, int>();
-
-            for (var i = 0; i < lines.Length; i++)
-            {
-                var input = lines[i].Split(':')[1].Split('|');
-
-                var winningNumbers = input[0].Split(' ').Where(s => s != "").Select(int.Parse);
-                var otherNumbers = input[1].Split(' ').Where(s => s != "").Select(int.Parse);
-
-                var numMatches = otherNumbers.Count(number => winningNumbers.Any(n => n == number));
-
-                cards.Add(i + 1, numMatches);
-            }
-
-            var firstSum = cards.Where(card => card.Value > 0).Sum(card => (int) Math.Pow(2, card.Value - 1));
-
-            var ownedCards = new Dictionary<int, int>();
-            cards.ToList().ForEach(card => ownedCards.Add(card.Key, 1));
-
-            for (var i = 1; i <= ownedCards.Count; i++)
-            {
-                for (var j = 1; j <= cards[i] && i + j <= ownedCards.Count; j++)
-                {
-                    ownedCards[i + j] += ownedCards[i];
-                }
-            }
-
-            var secondSum = ownedCards.Sum(card => card.Value);
-
-            Console.WriteLine($"First sum = {firstSum}, Second sum = {secondSum}");
-        }
+        ownedCards[i + j] += ownedCards[i];
     }
 }
+
+var secondSum = ownedCards.Sum(card => card.Value);
+
+Console.WriteLine($"First sum = {firstSum}, Second sum = {secondSum}");
