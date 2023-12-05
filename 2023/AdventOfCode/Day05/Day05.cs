109d40c30817d74a7c1faa399cb41fa3a5cbc3e7
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-05 16:38:41 +0100

    Non-working smart Day05

--- 2023/AdventOfCode/Day05/Day05.cs
+++ 2023/AdventOfCode/Day05/Day05.cs
@@ -6,7 +6,7 @@
         {
             var lines = File.ReadLines("input.txt").ToArray();
 
-            var seeds = lines[0].Split(':')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            var values = lines[0].Split(':')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(long.Parse).ToArray();
 
             var mapsLines = lines[1..];
@@ -22,59 +22,111 @@
             }
 
             var maps = mapInputs.Select(mapInput => new List<MapEntry>(mapInput.Skip(1)
-                .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray())
-                .Select(numbers => new MapEntry
-                    { DestinationStart = numbers[0], SourceStart = numbers[1], RangeLength = numbers[2] }))).ToList();
+                    .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray())
+                    .Select(numbers =>
+                        new MapEntry(new Interval(numbers[1], numbers[1] + numbers[2] - 1), numbers[0] - numbers[1]))))
+                .ToList();
 
             foreach (var map in maps)
             {
-                for (var i = 0; i < seeds.Length; i++)
+                for (var i = 0; i < values.Length; i++)
                 {
-                    var result = map.FirstOrDefault(entry =>
-                        seeds[i] >= entry.SourceStart && seeds[i] < entry.SourceStart + entry.RangeLength);
+                    var result = map.FirstOrDefault(entry => entry.SourceInterval.Contains(values[i]));
 
                     if (result is not null)
                     {
-                        seeds[i] = result.DestinationStart + (seeds[i] - result.SourceStart);
+                        values[i] += result.Offset;
                     }
                 }
             }
 
-            var seedGroups = lines[0].Split(':')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(long.Parse).ToArray().Chunk(2).ToArray();
+            var intervals = lines[0].Split(':')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse).ToArray().Chunk(2).ToArray()
+                .Select(group => new Interval(group[0], group[0] + group[1] - 1)).ToList();
 
-            var lowest = long.MaxValue;
-
-            foreach (var seedGroup in seedGroups)
+            foreach (var map in maps)
             {
-                Console.WriteLine($"Starting seedGroup {seedGroup[0]},{seedGroup[1]}");
-                for (var i = seedGroup[0]; i < seedGroup[0] + seedGroup[1]; i++)
+                var newIntervals = new List<Interval>();
+                foreach (var interval in intervals)
                 {
-                    var value = i;
-                    foreach (var map in maps)
-                    {
-                        var result = map.FirstOrDefault(entry =>
-                            value >= entry.SourceStart && value < entry.SourceStart + entry.RangeLength);
+                    // Check if there is a map entry that overlaps with the interval
+                    var match = map.FirstOrDefault(entry => entry.SourceInterval.HasOverlap(interval));
 
-                        if (result is not null)
-                        {
-                            value = result.DestinationStart + (value - result.SourceStart);
-                        }
+                    // If there isn't, the interval is unchanged
+                    if (match is null)
+                    {
+                        newIntervals.Add(interval);
+                        continue;
                     }
 
-                    if (value < lowest)
-                        lowest = value;
+                    // Else, keep non-overlapping bits unchanged but transform the overlapping part to new values
+                    var overlap = match.SourceInterval.GetOverlap(interval);
+                    var unchanged = interval.GetNonOverlap(overlap);
+                    newIntervals.AddRange(unchanged);
+
+                    overlap.MoveBy(match.Offset);
+                    newIntervals.Add(overlap);
                 }
+
+                var lowest = newIntervals.Min(interval => interval.Start);
+
+                intervals.Clear();
+                intervals.AddRange(newIntervals);
             }
 
-            Console.WriteLine($"Lowest location numbers (part 1, part 2) = {seeds.Min()}, {lowest}");
+            Console.WriteLine(
+                $"Lowest location numbers (part 1, part 2) = {values.Min()}, {intervals.Min(interval => interval.Start)}");
         }
     }
 
-    class MapEntry
+
+    internal class MapEntry(Interval sourceInterval, long offset)
     {
-        public long DestinationStart { get; set; }
-        public long SourceStart { get; set; }
-        public long RangeLength { get; set; }
+        public Interval SourceInterval { get; set; } = sourceInterval;
+        public long Offset { get; set; } = offset;
+    }
+
+    internal class Interval(long start, long end)
+    {
+        public long Start { get; set; } = start;
+        public long End { get; set; } = end;
+
+        public void MoveBy(long offset)
+        {
+            Start += offset;
+            End += offset;
+        }
+
+        public bool Contains(long number)
+        {
+            return number >= Start && number <= End;
+        }
+
+        public bool HasOverlap(Interval other)
+        {
+            return Start < other.End && other.Start < End;
+        }
+
+        public Interval GetOverlap(Interval other)
+        {
+            return new Interval(long.Max(Start, other.Start), long.Min(End, other.End));
+        }
+
+        public List<Interval> GetNonOverlap(Interval overlap)
+        {
+            var intervals = new List<Interval>();
+
+            if (Start < overlap.Start)
+            {
+                intervals.Add(new Interval(Start, overlap.Start - 1));
+            }
+
+            if (End > overlap.End)
+            {
+                intervals.Add(new Interval(overlap.End + 1, End));
+            }
+
+            return intervals;
+        }
     }
 }
