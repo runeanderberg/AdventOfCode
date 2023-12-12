3e69235b04ad3e9099135fea040ce04476595f9f
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-12 12:08:35 +0100

    Day12 part 2 added unfolding function and thoughts on how to solve the problem

--- 2023/AdventOfCode/Day12/Day12.cs
+++ 2023/AdventOfCode/Day12/Day12.cs
@@ -6,14 +6,39 @@ namespace Day12
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadLines("input.txt").ToList();
+            var lines = File.ReadLines("test_input.txt").ToList();
 
             var firstSum = lines.Sum(GetPossibleArrangementsByRow);
 
-            Console.WriteLine($"First sum = {firstSum}");
+            var unfoldedLines = lines.Select(Unfold);
+
+            // Brute-forcing the unfolded lines is essentially impossible, there are just too many combinations to check
+            // Ideas to solve the problem:
+            //  - Splitting the pattern into sub-problems based on .s and then multiply to get total number of possibilities per row
+            //      * Ex. ??.??.?? 1,1,1, can be split into 3 sub-problems (?? 1 => 2) which gives 2^3 as the final result
+            //      * One issue is that some lines, ex. ??#??#????##, can't be split that easily
+            //      * Another issue is that the number of groups separated by a . is not always the same as the number of contiguous group numbers, ex. ??.??.?? 1,1 is valid
+            //  - It is probably wise to try to figure out the matches of groups of only # with it's respective contiguous group before doing other calculations
+            //      * Ex. in ????.######..#####. 1,6,5, the matching is obvious and the problem can be reduced to sub-problem ???? 1
+            //      * Secondly, in .??..??...?##. 1,1,3, only the last group can match with the 3, which should also be figured out early
+
+            Console.WriteLine($"First sum = {firstSum}, second sum = {0}");
         }
 
-        
+        private static string Unfold(string row)
+        {
+            var data = row.Split(' ');
+            var pattern = new StringBuilder(data[0]);
+            var numbers = new StringBuilder(data[1]);
+            
+            for (var i = 0; i < 4; i++)
+            {
+                pattern.Append('?').Append(data[0]);
+                numbers.Append(',').Append(data[1]);
+            }
+
+            return pattern + " " + numbers;
+        }
 
         private static int GetPossibleArrangementsByRow(string row)
         {
@@ -27,7 +52,7 @@ namespace Day12
             return res;
         }
 
-        private static IEnumerable<string> Generate(string pattern)
+        private static List<string> Generate(string pattern)
         {
             var result = new List<string>();
 
