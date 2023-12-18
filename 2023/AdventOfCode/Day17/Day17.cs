dbe1c85d913d30b47630dabd5e2dd2a4684ec244
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-18 10:45:56 +0100

    Day17 part 1 fail+ Day18 part 1 solution

---
+++ 2023/AdventOfCode/Day17/Day17.cs
@@ -0,0 +1,79 @@
+﻿namespace Day17
+{
+    internal class Day17
+    {
+        private static int _maxRow;
+        private static int _maxCol;
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("example_input_1.txt").ToArray();
+
+            var input = new int[lines.Length, lines[0].Length];
+
+            for (var row = 0; row < lines.Length; row++)
+            {
+                for (var col = 0; col < lines[row].Length; col++)
+                {
+                    input[row, col] = lines[row][col] - '0';
+                }
+            }
+
+            _maxRow = input.GetLength(0);
+            _maxCol = input.GetLength(1);
+
+            // Start, top-left (0, 0), end bottom-right (maxRow - 1, maxCol - 1)
+
+            
+
+            // Printing stuff
+            for (var row = 0; row < _maxRow; row++)
+            {
+                for (var col = 0; col < _maxCol; col++)
+                {
+                    Console.Write(input[row, col]);
+                }
+
+                Console.Write('\n');
+                Console.ResetColor();
+            }
+        }
+
+        private static List<(int Row, int Col)> GetValidNextSteps(List<(int Row, int Col)> previousSteps)
+        {
+            var possibleSteps = new List<(int Row, int Col)>();
+
+            var (row, col) = previousSteps[^1];
+
+            possibleSteps.Add((row + 1, Col: col));
+            possibleSteps.Add((row - 1, Col: col));
+            possibleSteps.Add((Row: row, col + 1));
+            possibleSteps.Add((Row: row, col - 1));
+
+            if (previousSteps.Count > 1)
+            {
+                var previous = previousSteps[^2];
+                var diff = (Row: row - previous.Row, Col: col - previous.Col);
+
+                possibleSteps.RemoveAll(
+                    step => step.Row == row - diff.Row && step.Col == col - diff.Col);
+
+                if (previousSteps.Count > 3)
+                {
+                    var relevant = previousSteps[^4..];
+
+                    if (relevant.DistinctBy(step => step.Row).Count() == 4 ||
+                        relevant.DistinctBy(step => step.Col).Count() == 4)
+                    {
+                        possibleSteps.RemoveAll(step => step.Row == row + diff.Row && step.Col == col + diff.Col);
+                    }
+                }
+            }
+
+            // Remove steps that accidentally goes outside map
+            possibleSteps.RemoveAll(step => step.Row < 0 || step.Row >= _maxRow || step.Col < 0 || step.Col >= _maxCol);
+
+            return possibleSteps;
+        }
+    }
+}
---
+++ 2023/AdventOfCode/Day18/Day18.cs
@@ -0,0 +1,109 @@
+﻿namespace Day18
+{
+    internal class Day18
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("input.txt").ToArray();
+
+            var digInstructions = lines.Select(line => line.Split(' '))
+                .Select(split => (Direction: split[0], Length: int.Parse(split[1])));
+
+            // Start digging at (0, 0), store coordinates of dug spaces
+            var dugSpaces = new List<(int X, int Y)> { (0, 0) };
+            var currentX = 0;
+            var currentY = 0;
+
+            foreach (var (direction, length) in digInstructions)
+            {
+                var offsetX = 0;
+                var offsetY = 0;
+
+                switch (direction)
+                {
+                    case "U":
+                        offsetY--;
+                        break;
+                    case "D":
+                        offsetY++;
+                        break;
+                    case "L":
+                        offsetX--;
+                        break;
+                    case "R":
+                        offsetX++;
+                        break;
+                }
+
+                for (var i = 0; i < length; i++)
+                {
+                    currentX += offsetX;
+                    currentY += offsetY;
+                    dugSpaces.Add((currentX, currentY));
+                }
+            }
+
+            // Move dug spaces coordinates into map (+ translate coordinates as digging can have happened at negative X and Y)
+            var minX = dugSpaces.Min(space => space.X);
+            var maxX = dugSpaces.Max(space => space.X);
+            var minY = dugSpaces.Min(space => space.Y);
+            var maxY = dugSpaces.Max(space => space.Y);
+            var dugMap = new bool[maxX - minX + 1,maxY - minY + 1];
+            foreach (var (x, y) in dugSpaces)
+            {
+                dugMap[x - minX, y - minY] = true;
+            }
+
+            var lengthX = dugMap.GetLength(0);
+            var lengthY = dugMap.GetLength(1);
+
+            var queue = new Queue<(int X, int Y)>();
+            queue.Enqueue((lengthX / 2, lengthY / 2));
+            
+            while (queue.Count > 0)
+            {
+                var (x, y) = queue.Dequeue();
+
+                if (x < 0 || x >= lengthX || y < 0 || y >= lengthY)
+                {
+                    continue;
+                }
+
+                if (dugMap[x, y])
+                {
+                    continue;
+                }
+
+                dugMap[x, y] = true;
+
+                queue.Enqueue((x - 1, y));
+                queue.Enqueue((x + 1, y));
+                queue.Enqueue((x, y - 1));
+                queue.Enqueue((x, y + 1));
+            }
+            
+            for (var y = 0; y < lengthY; y++)
+            {
+                for (var x = 0; x < lengthX; x++)
+                {
+                    Console.Write(dugMap[x, y] ? '#' : '.');
+                }
+                Console.Write("\n");
+            }
+
+            var area = 0;
+            for (var x = 0; x < lengthX; x++)
+            {
+                for (var y = 0; y < lengthY; y++)
+                {
+                    if (dugMap[x, y])
+                    {
+                        area++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"First sum = {area}");
+        }
+    }
+}
