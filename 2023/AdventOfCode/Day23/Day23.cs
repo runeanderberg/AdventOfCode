e92e66a50cd47aaa47bd8d4e02fe3f173189592c
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-23 16:59:34 +0100

    Day23 part 2 still not working

--- 2023/AdventOfCode/Day23/Day23.cs
+++ 2023/AdventOfCode/Day23/Day23.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using Helpers;
+﻿using Helpers;
 
 namespace Day23
 {
@@ -33,21 +32,62 @@ namespace Day23
                 break;
             }
 
-            Console.WriteLine($"First sum = {FindLongestPathLength(map, start, end)}");
+            Console.WriteLine($"First sum = {FindLongestSlipperyPathLength(map, start, end)}");
 
-            for (var row = 0; row < rowLength; row++)
-            {
-                for (var col = 0; col < colLength; col++)
-                {
-                    if (map[row, col] is '>' or 'v')
-                        map[row, col] = '.';
-                }
-            }
-
-            Console.WriteLine($"Second sum = {FindLongestPathLength(map, start, end)}");
+            Console.WriteLine($"Second sum = {FindLongestNonSlipperyPathLength(map, start, end)}");
         }
 
-        private static int FindLongestPathLength(char[,] map, (int Row, int Col) start, (int Row, int Col) end)
+        private static int FindLongestSlipperyPathLength(char[,] map, (int Row, int Col) start, (int Row, int Col) end)
+        {
+            var rowLength = map.GetLength(0);
+            var colLength = map.GetLength(1);
+
+            var queue = new Queue<(int Row, int Col, List<(int Row, int Col)> PreviousSteps)>();
+            queue.Enqueue((start.Row, start.Col, new List<(int Row, int Col)>()));
+
+            var stepsMap = new int[rowLength, colLength];
+
+            while (queue.Count > 0)
+            {
+                var (row, col, previousSteps) = queue.Dequeue();
+
+                if (stepsMap[row, col] != 0 && stepsMap[row, col] + 2 >= previousSteps.Count)
+                {
+                    continue;
+                }
+
+                stepsMap[row, col] = previousSteps.Count;
+
+                var toCheck = new List<(int Row, int Col, List<(int Row, int Col)>)>();
+
+                switch (map[row, col])
+                {
+                    case '.':
+                        toCheck.Add((row - 1, col, new List<(int Row, int Col)>(previousSteps) { (row - 1, col) }));
+                        toCheck.Add((row + 1, col, new List<(int Row, int Col)>(previousSteps) { (row + 1, col) }));
+                        toCheck.Add((row, col - 1, new List<(int Row, int Col)>(previousSteps) { (row, col - 1) }));
+                        toCheck.Add((row, col + 1, new List<(int Row, int Col)>(previousSteps) { (row, col + 1) }));
+
+                        foreach (var step in toCheck.Where(step => step.Row >= 0 && step.Row < rowLength &&
+                                                                   step.Col >= 0 && step.Col < colLength &&
+                                                                   map[step.Row, step.Col] != '#'))
+                        {
+                            queue.Enqueue(step);
+                        }
+                        break;
+                    case '>':
+                        queue.Enqueue((row, col + 1, new List<(int Row, int Col)>(previousSteps) { (row, col + 1) }));
+                        break;
+                    case 'v':
+                        queue.Enqueue((row + 1, col, new List<(int Row, int Col)>(previousSteps) { (row + 1, col) }));
+                        break;
+                }
+            }
+            
+            return stepsMap[end.Row, end.Col];
+        }
+
+        private static int FindLongestNonSlipperyPathLength(char[,] map, (int Row, int Col) start, (int Row, int Col) end)
         {
             var rowLength = map.GetLength(0);
             var colLength = map.GetLength(1);
@@ -57,7 +97,7 @@ namespace Day23
 
             var stepsMap = new int[rowLength, colLength];
             var paths = new List<HashSet<(int Row, int Col)>>();
-
+            
             while (queue.Count > 0)
             {
                 var (row, col, previousSteps) = queue.Dequeue();
@@ -76,34 +116,25 @@ namespace Day23
                     paths.Add(previousSteps);
                 }
 
-                var toCheck = new List<(int Row, int Col, HashSet<(int Row, int Col)>)>();
-
-                switch (map[row, col])
+                var toCheck = new List<(int Row, int Col, HashSet<(int Row, int Col)>)>
                 {
-                    case '.':
-                        toCheck.Add((row - 1, col, new (previousSteps)));
-                        toCheck.Add((row + 1, col, new(previousSteps)));
-                        toCheck.Add((row, col - 1, new(previousSteps)));
-                        toCheck.Add((row, col + 1, new(previousSteps)));
+                    (row - 1, col, new(previousSteps)),
+                    (row + 1, col, new(previousSteps)),
+                    (row, col - 1, new(previousSteps)),
+                    (row, col + 1, new(previousSteps))
+                };
 
-                        foreach (var step in toCheck.Where(step => step.Row >= 0 && step.Row < rowLength &&
-                                                                   step.Col >= 0 && step.Col < colLength &&
-                                                                   map[step.Row, step.Col] != '#'))
-                        {
-                            queue.Enqueue(step);
-                        }
-                        break;
-                    case '>':
-                        queue.Enqueue((row, col + 1, new(previousSteps)));
-                        break;
-                    case 'v':
-                        queue.Enqueue((row + 1, col, new(previousSteps)));
-                        break;
+                foreach (var step in toCheck.Where(step => step.Row >= 0 && step.Row < rowLength &&
+                                                           step.Col >= 0 && step.Col < colLength &&
+                                                           map[step.Row, step.Col] != '#'))
+                {
+                    queue.Enqueue(step);
                 }
             }
-
+            
             var path = paths.MaxBy(path => path.Count);
 
+            Console.WriteLine();
             for (var row = 0; row < rowLength; row++)
             {
                 for (var col = 0; col < colLength; col++)
