1d9548a9953ba35b69037af5c5a8bcfe440b59d4
Rune Anderberg <rune.anderberg@gmail.com>
2023-12-23 12:10:25 +0100

    Day23 part 2 almost working solution

--- 2023/AdventOfCode/Day23/Day23.cs
+++ 2023/AdventOfCode/Day23/Day23.cs
@@ -1,4 +1,5 @@
-﻿using Helpers;
+﻿using System.Diagnostics;
+using Helpers;
 
 namespace Day23
 {
@@ -32,65 +33,89 @@ namespace Day23
                 break;
             }
 
+            Console.WriteLine($"First sum = {FindLongestPathLength(map, start, end)}");
 
-            var queue = new Queue<(int Row, int Col, List<(int Row, int Col)> PreviousSteps)>();
-            queue.Enqueue((start.Row, start.Col, new List<(int Row, int Col)>()));
+            for (var row = 0; row < rowLength; row++)
+            {
+                for (var col = 0; col < colLength; col++)
+                {
+                    if (map[row, col] is '>' or 'v')
+                        map[row, col] = '.';
+                }
+            }
+
+            Console.WriteLine($"Second sum = {FindLongestPathLength(map, start, end)}");
+        }
+
+        private static int FindLongestPathLength(char[,] map, (int Row, int Col) start, (int Row, int Col) end)
+        {
+            var rowLength = map.GetLength(0);
+            var colLength = map.GetLength(1);
+
+            var queue = new Queue<(int Row, int Col, HashSet<(int Row, int Col)> PreviousSteps)>();
+            queue.Enqueue((start.Row, start.Col, new HashSet<(int Row, int Col)>()));
 
             var stepsMap = new int[rowLength, colLength];
+            var paths = new List<HashSet<(int Row, int Col)>>();
 
             while (queue.Count > 0)
             {
                 var (row, col, previousSteps) = queue.Dequeue();
-                
-                if (stepsMap[row, col] != 0 && stepsMap[row, col] + 2 >= previousSteps.Count)
-                {
+
+                if (previousSteps.Contains((row, col)))
                     continue;
-                }
+
+                if (stepsMap[row, col] != 0 && previousSteps.Count <= stepsMap[row, col])
+                    continue;
 
                 stepsMap[row, col] = previousSteps.Count;
+                previousSteps.Add((row, col));
 
-                var toCheck = new List<(int Row, int Col, List<(int Row, int Col)>)>();
-                
-                if (map[row, col] == '.')
+                if ((row, col) == end)
                 {
-                    toCheck.Add((row - 1, col, new List<(int Row, int Col)>(previousSteps) { (row - 1, col) }));
-                    toCheck.Add((row + 1, col, new List<(int Row, int Col)>(previousSteps) { (row + 1, col) }));
-                    toCheck.Add((row, col - 1, new List<(int Row, int Col)>(previousSteps) { (row, col - 1) }));
-                    toCheck.Add((row, col + 1, new List<(int Row, int Col)>(previousSteps) { (row, col + 1) }));
-
-                    foreach (var step in toCheck.Where(step => step.Row >= 0 && step.Row < rowLength &&
-                                                               step.Col >= 0 && step.Col < colLength &&
-                                                               map[step.Row, step.Col] != '#'))
-                    {
-                        queue.Enqueue(step);
-                    }
-
-                    continue;
+                    paths.Add(previousSteps);
                 }
+
+                var toCheck = new List<(int Row, int Col, HashSet<(int Row, int Col)>)>();
 
                 switch (map[row, col])
                 {
+                    case '.':
+                        toCheck.Add((row - 1, col, new (previousSteps)));
+                        toCheck.Add((row + 1, col, new(previousSteps)));
+                        toCheck.Add((row, col - 1, new(previousSteps)));
+                        toCheck.Add((row, col + 1, new(previousSteps)));
+
+                        foreach (var step in toCheck.Where(step => step.Row >= 0 && step.Row < rowLength &&
+                                                                   step.Col >= 0 && step.Col < colLength &&
+                                                                   map[step.Row, step.Col] != '#'))
+                        {
+                            queue.Enqueue(step);
+                        }
+                        break;
                     case '>':
-                        queue.Enqueue((row, col + 1, new List<(int Row, int Col)>(previousSteps) { (row, col + 1) }));
+                        queue.Enqueue((row, col + 1, new(previousSteps)));
                         break;
                     case 'v':
-                        queue.Enqueue((row + 1, col, new List<(int Row, int Col)>(previousSteps) { (row + 1, col) }));
+                        queue.Enqueue((row + 1, col, new(previousSteps)));
                         break;
                 }
             }
 
-            //for (var row = 0; row < rowLength; row++)
-            //{
-            //    for (var col = 0; col < colLength; col++)
-            //    {
-            //        Console.BackgroundColor = stepsMap[row, col] > 0 ? ConsoleColor.DarkGreen : ConsoleColor.Black;
-            //        Console.Write(map[row, col]);
-            //    }
-            //    Console.ResetColor();
-            //    Console.Write('\n');
-            //}
+            var path = paths.MaxBy(path => path.Count);
 
-            Console.WriteLine($"First sum = {stepsMap[end.Row, end.Col]}");
+            for (var row = 0; row < rowLength; row++)
+            {
+                for (var col = 0; col < colLength; col++)
+                {
+                    Console.BackgroundColor = path!.Contains((row, col)) ? ConsoleColor.DarkGreen : ConsoleColor.Black;
+                    Console.Write(map[row, col]);
+                }
+                Console.ResetColor();
+                Console.Write('\n');
+            }
+
+            return stepsMap[end.Row, end.Col];
         }
     }
 }
